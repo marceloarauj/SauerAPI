c18a10994caec5805181f7591bb3c939ca43fa26
marcelo-araujo <marcelo.araujo@avonale.com.br>
2020-04-23 12:13:47 -0300

    ExameController

--- ProjetoEngSoftware/DTO/PedidoExameDTO.cs
+++ ProjetoEngSoftware/DTO/PedidoExameDTO.cs
@@ -1,7 +1,14 @@
+using System;
+
 namespace ProjetoEngSoftware.DTO
 {
     public class PedidoExameDTO
     {
-        
+        public int IdExame {get;set;}
+        public string CpfCliente {get;set;}
+        public string IdMedico {get;set;}
+        public string HipoteseDiagnostica {get;set;}
+        public string Recomendacoes {get;set;}
+        public DateTime DataExame {get;set;}
     }
 }
--- ProjetoEngSoftware/Entity/PacienteEntity.cs
+++ ProjetoEngSoftware/Entity/PacienteEntity.cs
@@ -9,8 +9,8 @@ namespace ProjetoEngSoftware.Entity
         public void Configure(EntityTypeBuilder<Paciente> builder)
         {
             builder.ToTable("tb_paciente");
-            builder.HasKey(x => x.Id);
-            builder.Property(x => x.Id).HasColumnName("id_paciente").ValueGeneratedOnAdd();
+            builder.HasKey(x => x.Cpf);
+            builder.Property(x => x.Cpf).HasColumnName("cpf_paciente");
             builder.Property(x => x.DataNascimento).HasColumnName("dt_nascimento");
             builder.Property(x => x.Sexo).HasColumnName("tp_sexo");
             builder.HasOne(x => x.Etnia);
